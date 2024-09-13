96899ec6ba20fae26bf82d4bd20be5bf42fff6ae
Valdeir Sampaio <saparavaldeir@gmail.com>
2024-09-13 19:18:32 -0400

    Configurando primeira model

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default= True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.28.177.56']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contrib'
 ]
 
 MIDDLEWARE = [
--- contrib/models.py
+++ contrib/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Perfil(models.Model):
+    nome = models.CharField(max_length=250, blank=None,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    
+    
+    def __str__(self) ->str:
+        return self.nome
